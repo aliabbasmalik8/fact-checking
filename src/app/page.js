99c6ef651c54d17361041c80bceb098c50a0b1ce
Ali Abbas Malik <aliabbasmalik8@gmail.com>
2023-10-05 16:48:10 +0500

    styling and description fixes

--- src/app/page.js
+++ src/app/page.js
@@ -3,43 +3,8 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Fact Checkers and Retrievers
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <main className="flex justify-center min-h-screen flex-col items-center">
+      <div className=" mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
           href="/fact-checking"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -53,7 +18,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            A simple fact checker using Wikipedia Query Runner provided by LangChain
+            Uses Wikipedia Query Runner provided by LangChain JS to get the data from Wikepedia and then use it to train OpenAI and verify the statement provided.
           </p>
         </Link>
 
@@ -70,11 +35,11 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Enter one question and one url to scrape the url and verify your facts
+            Scrapes the data from the entered url and then feed it to OpenAI, which trains and then answer your question.
           </p>
         </Link>
 
-        <Link
+        {/* <Link
           href="/serp-fact-checking"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -89,7 +54,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Uses Serp API to collect data and verify your statement
           </p>
-        </Link>
+        </Link> */}
 
         <Link
           href="/wiki-fact-checking"
@@ -104,7 +69,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Uses Wikipedia as an information source to verify statements
+            Use Wikiepedia to extract the data from Wikipedia using Retriever in python about your question and then train OpenAI on it to answer or verify it accordingly.
           </p>
         </Link>
         <Link
@@ -120,7 +85,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Scraps links from Wikipedia according to your statments for data and verify your facts
+            Searches your question on Wikipedia and then scrapes the data from the provided links to train OpenAI, which then answer the question according to the scraped data
           </p>
         </Link>
         <Link
@@ -136,7 +101,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Scraps links from Wikipedia according to your statments for data and stores it into a vector store for later retrieval
+            Scraps links from Wikipedia according to your statments for data and OpenAI trains on that data to answer the provuded questions. The scraped data is stored in a vector store for later retrieval
           </p>
         </Link>
       </div>
