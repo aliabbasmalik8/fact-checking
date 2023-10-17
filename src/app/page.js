c9c7e49e75ed22885a381ff232e3cb546890d8d5
Ali Abbas Malik <aliabbasmalik8@gmail.com>
2023-10-17 18:20:23 +0500

    added new route

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -18,7 +18,9 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Uses Wikipedia Query Runner provided by LangChain JS to get the data from Wikepedia and then use it to train OpenAI and verify the statement provided.
+            Uses Wikipedia Query Runner provided by LangChain JS to get the data
+            from Wikepedia and then use it to train OpenAI and verify the
+            statement provided.
           </p>
         </Link>
 
@@ -35,7 +37,8 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Scrapes the data from the entered url and then feed it to OpenAI, which trains and then answer your question.
+            Scrapes the data from the entered url and then feed it to OpenAI,
+            which trains and then answer your question.
           </p>
         </Link>
 
@@ -69,7 +72,9 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Use Wikiepedia to extract the data from Wikipedia using Retriever in python about your question and then train OpenAI on it to answer or verify it accordingly.
+            Use Wikiepedia to extract the data from Wikipedia using Retriever in
+            python about your question and then train OpenAI on it to answer or
+            verify it accordingly.
           </p>
         </Link>
         <Link
@@ -85,7 +90,9 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Searches your question on Wikipedia and then scrapes the data from the provided links to train OpenAI, which then answer the question according to the scraped data
+            Searches your question on Wikipedia and then scrapes the data from
+            the provided links to train OpenAI, which then answer the question
+            according to the scraped data
           </p>
         </Link>
         <Link
@@ -101,7 +108,9 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Scraps links from Wikipedia according to your statments for data and OpenAI trains on that data to answer the provuded questions. The scraped data is stored in a vector store for later retrieval
+            Scraps links from Wikipedia according to your statments for data and
+            OpenAI trains on that data to answer the provuded questions. The
+            scraped data is stored in a vector store for later retrieval
           </p>
         </Link>
         <Link
@@ -117,7 +126,10 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Uses the Google Search API in python to search the provided question on Google and then scrapes the data from the first link using Beautiful Soup. The scraped data is then given to OpenAI which answers the question accordingly.
+            Uses the Google Search API in python to search the provided question
+            on Google and then scrapes the data from the first link using
+            Beautiful Soup. The scraped data is then given to OpenAI which
+            answers the question accordingly.
           </p>
         </Link>
         <Link
@@ -133,7 +145,11 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Uses the Serp API in pytho to get the links from the Google Search Engine and then scrapes the data from the first link using Beautiful Soup. The data scraped is provided to OpenAI for analyzing and training which then answers the question and save it to memory for future retrieval.
+            Uses the Serp API in pytho to get the links from the Google Search
+            Engine and then scrapes the data from the first link using Beautiful
+            Soup. The data scraped is provided to OpenAI for analyzing and
+            training which then answers the question and save it to memory for
+            future retrieval.
           </p>
         </Link>
         <Link
@@ -149,7 +165,28 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Uses the Google Search API in python to search the provided question appended with +wikepedia on Google and then scrapes the data from the first/wikepedia link using Selenium URL Document Loader of LangChain. The scraped data is then given to OpenAI which answers the question accordingly.
+            Uses the Google Search API in python to search the provided question
+            appended with +wikepedia on Google and then scrapes the data from
+            the first/wikepedia link using Selenium URL Document Loader of
+            LangChain. The scraped data is then given to OpenAI which answers
+            the question accordingly.
+          </p>
+        </Link>
+        <Link
+          href="/wiki-duck-duck-go-search"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            WikiSearch And Duck Duck Go Search with Zero Shot Agent{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Uses the Wikipedia Search tool and Duck Duck Go search engine tool
+            to verify the provided question.
           </p>
         </Link>
       </div>
---
+++ src/app/wiki-duck-duck-go-search/page.js
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+
+const FactChecking = () => {
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
+  const [responseTime, setResponseTime] = useState(0);
+
+  const handleAsk = async (question) => {
+    setResponseTime(0);
+    try {
+      const response = await fetch(
+        "http://localhost:8000/new_fact_check/message",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ question, whatsapp_number: "12345678" }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Response:", data); // Add this line
+        setResult(data.answer);
+        setResponseTime(data.responseTime);
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const getResult = async () => {
+    setResult("");
+    if (!query) return;
+
+    setLoading(true);
+    await handleAsk(query);
+
+    setLoading(false);
+  };
+
+  return (
+    <main className="min-h-screen pt-12 w-[1000px] max-w-full px-4 mx-auto">
+      <div className="flex justify-center py-4">
+        <h1 className="text-[16px] font-bold">
+          WikiSearch And Duck Duck Go Search with Zero Shot Agent
+        </h1>
+      </div>
+      <div>
+        <textarea
+          onChange={(e) => setQuery(e.target.value)}
+          className="text-black w-full h-32 p-2 outline-none resize-none"
+        />
+        <div className="flex justify-end">
+          <button
+            onClick={getResult}
+            className="flex bg-slate-900 text-white p-2 justify-center w-32"
+            disabled={loading}
+          >
+            {!loading && "Get Result"}
+            {loading && <span className="animate-spin h-5 w-5 ml-3 bg-white" />}
+          </button>
+        </div>
+      </div>
+      {responseTime !== 0 && (
+        <div className="mt-5">Response Time: {responseTime} ms</div>
+      )}
+      <div className="mt-8" style={{ whiteSpace: "pre-line" }}>
+        {result}
+      </div>
+    </main>
+  );
+};
+
+export default FactChecking;
